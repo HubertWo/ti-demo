ff6345f8bfd0d6dbb65d9dfadeb6a7474a4f7239
Hubert Wo <2518652+HubertWo@users.noreply.github.com>
2023-05-29 10:49:38 +0200

    Empty project

---
+++ src/main/java/com/github/hubertwo/ti/App.java
@@ -0,0 +1,6 @@
+package com.github.hubertwo.ti;
+
+/**
+ * @see <a href="https://github.com/HubertWo/ti-demo">Repository</a>
+ */
+public class App {}
---
+++ src/test/java/com/github/hubertwo/ti/AppTest.java
@@ -0,0 +1,13 @@
+package com.github.hubertwo.ti;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+class AppTest {
+
+  @Test
+  void dummy() {
+    assertThat(2 + 2).isEqualTo(4);
+  }
+}
